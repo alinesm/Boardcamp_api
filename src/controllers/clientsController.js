999bd8d765abdae1fc21e6d1c63913163e64b451
Aline Moura <alinesouzamoura_@hotmail.com>
2023-02-10 14:23:08 -0300

    fix  update client 4

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -49,20 +49,36 @@ export async function getClienttById(req, res) {
 
 export async function updateClient(req, res) {
   const { id } = req.params;
+  const customerId = Number(id);
   const { name, phone, cpf, birthday } = res.locals.client;
 
+  if (!customerId || customerId < 1 || !Number.isSafeInteger(customerId)) {
+    return res.sendStatus(400);
+  }
+
   const checkCustomerId = await db.query(
-    `SELECT name FROM customers WHERE id= '${id}'`
+    `SELECT name FROM customers WHERE id= '${customerId}'`
   );
-  // console.log(checkCustomerId.rows[0]);
+
   if (!checkCustomerId.rows[0])
     return res.status(404).send("Esse cliente não existe");
 
+  const existingCpf = await db.query(
+    "SELECT * FROM customers WHERE cpf = $1 AND id <> $2",
+    [cpf, customerId]
+  );
+  if (existingCpf.rowCount > 0) {
+    return res.sendStatus(409);
+  }
+
   try {
-    await db.query(
-      'UPDATE "customers" SET "name" = $1, "phone" = $2, "cpf" = $3, "birthday" = $4 WHERE id = $5',
-      [name, phone, cpf, birthday, id]
+    const updateClient = await db.query(
+      "UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5",
+      [name, phone, cpf, birthday, customerId]
     );
+    if (updateClient.rowCount === 0) {
+      return res.status(400).send("nao foi possivel atualizar");
+    }
     return res.status(200).send("OK");
   } catch (error) {
     console.log(error.message);
